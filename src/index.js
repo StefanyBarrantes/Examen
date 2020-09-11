1e7b4fca4243cb87d6c6839883ee8e112c6e4e5f
Stef <barrantesstefany@hotmail.com>
2020-09-11 07:33:43 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ const bode = require('body-parser')
 
 const server = express()
 
-const PORT = 3500
+//const PORT = 3500
 
 //settings
-server.set('port', PORT)
+//server.set('port', PORT)
 server.set('views', path.join(__dirname, '/views'))
 server.engine('html', ejs.renderFile)
 server.use(bode.urlencoded({ extended: false }))
@@ -19,8 +19,8 @@ server.use(routes)
 
 
 //server
-server.listen(server.get('port'), () => {
+/*server.listen(server.get('port'), () => {
     console.log(`Server started at http://localhost:${server.get('port')}`)
-})
+})*/
 
 
